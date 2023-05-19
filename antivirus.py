20a9f5becce6660ef93de3cee5bc6cde72ec95c7
robbyrca <rcuenca.alt@gmail.com>
2023-05-19 18:40:02 +0200

    Terminamos de hacer más funciones

--- antivirus.py
+++ antivirus.py
@@ -1,6 +1,6 @@
 #Importamos las librerias necesarias para el proyecto
-import shutil,os,json, time, datetime
-#import mysql.connector, requests
+import shutil,os,json, time, datetime, requests
+import mysql.connector
 from urllib import response
 from pathlib import Path
 
@@ -9,6 +9,15 @@ file_here = '/'
 file_temp = '/temp'
 file_quarantine = '/cuarentena'
 file_revised = '/revisado'
+
+#Definimos las variables para conectar con la base de datos
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="robbyrca",
+  password="QWEqwe123!",
+  database="antivirus",
+  auth_plugin='mysql_native_password'
+)
 
 #Definimos la función copiar
 def copiar(carpeta_origen, carpeta_desti):
@@ -44,76 +53,73 @@ def obtener_id(file):
         "accept": "application/json",
         "x-apikey": "206706e5d63a9393a5786e3191ba9c471dcbb00305f4a32d49de38c45f20c4c7"
     }
-    timesleepcount = timesleepcount + 1
-    if timesleepcount == 5:
-        print('Control de tiempo de 60 segundos')
-        time.sleep(60)
-        timesleepcount=0
+    response = requests.post(url, files=files, headers=headers)
+    if(response.status_code == 429):
+        print("Error de cuota excedida")
+        print("Codigo de error : " + str(response.status_code))
+        pause()
+        id = obtener_id(ruta)
+        return id
+    if response.status_code == 200:
+        jsonresp = response.json()
+        id = jsonresp.get("data").get("id")
+        return id
     else:
-        response = requests.post(url, files=files, headers=headers)
-        if(response.status_code == 429):
-            print("Error de cuota excedida")
-            print("Codigo de error : " + str(response.status_code))
-            exit()
-        if response.status_code == 200:
-            jsonresp = response.json()
-            idget = jsonresp.get("data").get("id")
-        else:
-            print ("No s'ha pogut obtenir la URL :(")
-            print ("ERROR al pujar el archiu :!")
-            print ("Status code: " + str(response.status_code))
+        print ("No s'ha pogut obtenir la URL :(")
+        print ("ERROR al pujar el archiu :!")
+        print ("Status code: " + str(response.status_code))
 
 #Definimos la función que va a enviar y recoger el id del archivo >32
 def obtener_id32(file):
-        global timesleepcount
-        global buclebig
         files = {"file": open(file, "rb")}
         url = "https://www.virustotal.com/api/v3/files/upload_url"
         headers = {
             "accept": "application/json",
             "x-apikey": "206706e5d63a9393a5786e3191ba9c471dcbb00305f4a32d49de38c45f20c4c7"
         }
-        timesleepcount = timesleepcount + 1
-        if timesleepcount == 5:
-            print('Control de tiempo de 60 segundos')
-            time.sleep(60)
-            timesleepcount=0
+
+        response = requests.get(url, headers=headers)
+        if(response.status_code == 429):
+            print("Error de cuota excedida :! o Error de demasiadas solicitudes controlate ;)")
+            print("Codigo de error : " + str(response.status_code))
+            pause()
+            id = obtener_id32(file)
+            return id
+
+        if response.status_code == 200:
+            result = response.json()
+            url_upload = result.get("data")
 
         else:
-            response = requests.get(url, headers=headers)
-            if(response.status_code == 429):
-                print("Error de cuota excedida :! o Error de demasiadas solicitudes controlate ;)")
-                print("Codigo de error : " + str(response.status_code))
-                exit()
-
-            if response.status_code == 200:
-                result = response.json()
-                url_upload = result.get("data")
-
-            else:
-                print ("No s'ha pogut obtenir la URL :(")
-                print ("ERROR al pujar el archiu :!")
-                print ("Status code: " + str(response.status_code))
+            print ("No s'ha pogut obtenir la URL :(")
+            print ("ERROR al pujar el archiu :!")
+            print ("Status code: " + str(response.status_code))
         
-            #Obtenim una id
-            response = requests.post(url_upload, files=files, headers=headers)
-            if(response.status_code == 429):
-                print("Error de cuota excedida :! o Error de demasiadas solicitudes controlate ;)")
-                print("Codigo de error : " + str(response.status_code))
-                exit()
+        #Obtenim una id
+        response = requests.post(url_upload, files=files, headers=headers)
+        if(response.status_code == 429):
+            print("Error de cuota excedida :! o Error de demasiadas solicitudes controlate ;)")
+            print("Codigo de error : " + str(response.status_code))
+            exit()
 
-            if response.status_code == 200:
-                result = response.json()
-                idbig = result.get("data").get("id")
+        if response.status_code == 200:
+            result = response.json()
+            id = result.get("data").get("id")
+            return id
 
 #Definimos la función que va recuperar el array de los archivos de la carpeta
 def recorrer_carpeta (carpeta):
     rutas = []
+    archivos = []
     for nombre_archivo in os.listdir(carpeta):
+        archivos.append(nombre_archivo)
         ruta = os.path.join(carpeta, nombre_archivo)
         if os.path.isfile(ruta):
             rutas.append(ruta)
-    return rutas
+    export_recorrido = []
+    export_recorrido[0] = rutas
+    export_recorrido[1] = archivos
+    return export_recorrido
 
 #Definimos la función que va a comprobar si un archivo es mayor que 32MB
 def comprobar_tamaño(ruta_archivo):
@@ -122,11 +128,94 @@ def comprobar_tamaño(ruta_archivo):
         tamano_archivo = os.path.getsize(ruta_archivo)
         if tamano_archivo >= tamano_minimo:
             return True
-    
-copiar('/Users/ruben/Documents/Audiolibros','/Users/ruben/Documents/pruebita')
-ruta = recorrer_carpeta('/Users/ruben/Documents/pruebita')
-for ruta in recorrer_carpeta:
-       if comprobar_tamaño (ruta):
-           obtener_id32(ruta)
-       else:
-           obtener_id(ruta)
+
+def mover(carpeta_origen, carpeta_desti):
+    # Comprovar si la carpeta de desti existeix, si no, crear-la
+    if not os.path.exists(carpeta_desti):
+        os.makedirs(carpeta_desti)
+
+    # Per a cada carpeta, subcarpeta i fitxer que es trobi a la carpeta origen
+    for carpeta, subcarpetes, fitxers in os.walk(carpeta_origen):
+        for fitxer in fitxers:
+            # Obtenir la ruta completa del fitxer
+            ruta_completa = os.path.join(carpeta, fitxer)
+            # Copiar el fitxer a la carpeta de desti
+            shutil.move(ruta_completa, carpeta_desti)
+
+#Definimos la función que va hacer una pausa de 60 segundos
+def pause():
+    segundos = 0
+    while segundos < 60:
+        print(segundos)
+        segundos += 1
+        time.sleep(1)
+
+#Definimos la función para analizar la id
+def analizar(id):
+    global bucle
+    global timesleepcount
+    url = "https://www.virustotal.com/api/v3/analyses/"+id
+    headers = {
+        "accept": "application/json",
+        "x-apikey": "206706e5d63a9393a5786e3191ba9c471dcbb00305f4a32d49de38c45f20c4c7"
+    }
+    response = requests.get(url, headers=headers)
+    if(response.status_code == 429):
+        print("Error de cuota excedida :! o Error de demasiadas solicitudes controlate ;)")
+        print("Codigo de error : " + str(response.status_code))
+        result = analizar(id)
+        return result
+    if response.status_code == 200:
+        #print(response.text)
+        jsonresp = response.json()
+        if jsonresp.get("data").get("attributes").get("status") != "queued":  
+            malget = jsonresp.get("data").get("attributes").get("stats").get("malicious")
+            #print (malget)
+            if  malget>0:
+                print('Archivo malicioso detectado!')
+                #Determinem result = 1 (virus)
+                result = 1
+                return result
+            else:
+                #Determinem result = 0 (freevir)
+                result = 0
+                return result
+        else:
+            print ("No s'ha pogut obtenir la URL :(")
+            print ("ERROR al pujar el archiu :!")
+            print ("Status code: " + str(response.status_code))
+
+#Definimos la función para guardar en la base de datos
+def sql(result, archivo):
+    if result == 1:
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO cuarentena (path, filename) VALUES (%s, %s)"
+        val = ('/var/www/html/antivirus/archivos', archivo)
+        mycursor.execute(sql, val)
+        mydb.commit()
+    else:   
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO archivos (path, filename) VALUES (%s, %s)"
+        val = ('/var/www/html/antivirus/cuarentena',archivo)
+        mycursor.execute(sql, val)
+        mydb.commit()
+
+
+#PROGRAMA PRINCIPAL
+copiar('/Users/ruben/Documents/revista','/Users/ruben/Documents/revistita')
+export_recorrido = recorrer_carpeta('/Users/ruben/Documents/revistita')
+rutas = export_recorrido[0]
+archivos = export_recorrido[1]
+posicion = 0
+for ruta in rutas:
+        if comprobar_tamaño (ruta):
+           print('obteniendo id: '+ruta)
+           id = obtener_id32(ruta)
+           result = analizar(id)
+           sql(result, archivos[posicion])
+        else:
+           print('obteniendo id: '+ruta)
+           id = obtener_id(ruta)
+           result = analizar(id)
+           sql(result,archivos[posicion])
+        posicion=posicion+1
