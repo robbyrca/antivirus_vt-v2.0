f5d1b203b034c21c7d41f1029ba6c01cf95f4bef
robbyrca <rcuenca.alt@gmail.com>
2023-05-28 12:33:37 +0200

    provar inversa

--- antivirus.py
+++ antivirus.py
@@ -236,7 +236,7 @@ posicion = 0
 for ruta in rutas:
         if comprobar_tamaño (ruta):
             logs(1,ruta)
-            id = obtener_id(ruta)
+            id = obtener_id32(ruta)
             result = analizar(id)
             if result == 1:
                 mover(file_temp,file_result1)
@@ -248,7 +248,7 @@ for ruta in rutas:
                 logs(2,ruta)
         else:
             logs(1,ruta)
-            id = obtener_id32(ruta)
+            id = obtener_id(ruta)
             result = analizar(id)
             sql(result,archivos[posicion],foranea)
             logs(2,ruta)
