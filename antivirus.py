0e50f1005c9f4c04c86abb3cf35bd00fae5819ba
robbyrca <rcuenca.alt@gmail.com>
2023-05-28 15:42:42 +0200

    probar completando codigo

--- antivirus.py
+++ antivirus.py
@@ -265,6 +265,12 @@ for ruta in rutas:
             logs(1,ruta)
             id = obtener_id(ruta)
             result = analizar(id)
-            sql(result,archivos[posicion],foranea)
-            logs(2,ruta)
+            if result == 1:
+                mover(file_temp,file_result1)
+                sql(result,archivos[posicion], foranea)
+                logs(3,ruta)
+            else:
+                mover(file_temp, file_result0) 
+                sql(result,archivos[posicion],foranea)
+                logs(2,ruta)
         posicion=posicion+1
